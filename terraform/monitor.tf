5b525d556fb1ac5ee673ee382ade4a84bd11941a
yuji0276 <yujikami0110@gmail.com>
2026-08-24 01:51:47 +0900

    エージェントログ用VMを作成

---
+++ terraform/monitor.tf
@@ -0,0 +1,23 @@
+resource "sakura_disk" "log" {
+  name              = "log-disk"
+  source_archive_id = data.sakura_archive.ubuntu.id
+  size              = var.disk_size_log
+}
+
+resource "sakura_server" "log" {
+  name        = "log"
+  disks       = [sakura_disk.log.id]
+  description = "エージェントのログ回収用VM"
+  tags        = ["monitor"]
+
+  network_interface = [{
+    upstream         = sakura_vswitch.private.id
+    packet_filter_id = sakura_packet_filter.private_in.id
+  }]
+
+  disk_edit_parameter = {
+    hostname        = "log"
+    ssh_key_ids     = [sakura_ssh_key.main.id]
+    disable_pw_auth = true
+  }
+}
